96a4dd718937dbd4432c172981353fcfe3b89660
Francesca Pompi <francesca.pompi93@gmail.com>
2022-01-03 17:33:00 +0100

    step 2

--- src/main/java/org/generation/italy/controller/BestController.java
+++ src/main/java/org/generation/italy/controller/BestController.java
@@ -1,5 +1,6 @@
 package org.generation.italy.controller;
 
+import java.util.Arrays;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,5 +17,18 @@ public class BestController {
 		return "bestOf";
 	}
 	
+	@GetMapping("/movies")
+	public String movies(Model model) {
+		model.addAttribute("movies", Arrays.asList("House of Gucci", "Crudelia", "Dune", "No time to die", "Eternals", "Spider-man - No way home"));
+
+		return "movies";		
+	}
+	
+	@GetMapping("/songs")
+	public String songs(Model model) {
+		model.addAttribute("songs", Arrays.asList("Saturday - Twenty One Pilots", "Take My Brath - The Weekend", "Dopamine - Purple Disco Machine", "Easy On Me - Adele"));
+		
+		return "songs";		
+	}
 	
 }
